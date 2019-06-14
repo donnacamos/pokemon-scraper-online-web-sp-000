ecf3f339292f4c3fb2b736297a0c20bd03ec52d5
Donna Amos <donnacamos.developer@gmail.com>
2019-06-14 21:11:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,11 +3,8 @@ class Pokemon
   attr_accessor :name, :type, :db 
   attr_reader :id 
   
-  def initialize(name, type, db, id) 
-    @name = name 
-    @type = type 
-    @db = db 
-    @id = id 
+  def initialize(keywords) 
+    
   end
 
 end 
