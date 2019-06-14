c3278415ed6faa522903f3efa1fa5fc9cf6338d2
Donna Amos <donnacamos.developer@gmail.com>
2019-06-14 21:27:08 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,4 +15,6 @@ class Pokemon
     pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", [num]) 
    new_pokemon = self.new(pokemon) 
    new_pokemon.id = pokemon[0][0]
+   new_pokemon.name = pokemon[0][1]
+   new_pokemon.type = pokemon[0][2]
 end 
